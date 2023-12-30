c8e0072fbefde589d3620ad3f0689f6ed14e4745
Rayapati99 <rayapati.anil99@gmail.com>
2023-12-30 13:07:26 +0530

    data ingestion commited

--- src/exception.py
+++ src/exception.py
@@ -18,12 +18,6 @@ class CustomException(Exception):
         return self.error_message
         
         
-if __name__=='__main__':
-    try:
-        a=1/0
-    except Exception as e:
-        logging.info(e)
-        raise CustomException(e,sys)
-    
+
         
         
